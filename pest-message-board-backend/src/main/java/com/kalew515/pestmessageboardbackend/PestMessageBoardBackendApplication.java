e0b5ebb83805564863d37cbfa9fa773b8023255d
kalew515 <ilovewk51@gmail.com>
2021-01-31 17:23:11 +0800

    update

---
+++ pest-message-board-backend/src/main/java/com/kalew515/pestmessageboardbackend/PestMessageBoardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kalew515.pestmessageboardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PestMessageBoardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PestMessageBoardBackendApplication.class, args);
+    }
+
+}
---
+++ pest-message-board-backend/src/test/java/com/kalew515/pestmessageboardbackend/PestMessageBoardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalew515.pestmessageboardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PestMessageBoardBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
